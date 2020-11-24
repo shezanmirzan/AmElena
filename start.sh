dc319e8914fd4ded4eddc5584a4abbac1427d0ed
shezanmirzan <shezanmirzan@github.com>
2020-11-24 15:52:10 -0500

    updated start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,3 @@ export FLASK_DEBUG=1
 export APP_CONFIG_FILE=src/server/constants.py
 python -m flask run
 
-
-#set FLASK_APP=src\server\requesthandler.py
-#set FLASK_DEBUG=1
-#set APP_CONFIG_FILE=src\server\constants.py
-#python -m flask run
