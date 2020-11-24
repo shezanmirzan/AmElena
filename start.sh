411455dc6408d5a529b94971d344d2c41710096d
s-shashidhar <saurabh.shashidhar@gmail.com>
2020-11-24 12:16:02 -0500

    Added demo and presentation video

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ export APP_CONFIG_FILE=src/server/constants.py
 python -m flask run
 
 
-set FLASK_APP=src\server\requesthandler.py
-set FLASK_DEBUG=1
-set APP_CONFIG_FILE=src\server\constants.py
-python -m flask run
+#set FLASK_APP=src\server\requesthandler.py
+#set FLASK_DEBUG=1
+#set APP_CONFIG_FILE=src\server\constants.py
+#python -m flask run
